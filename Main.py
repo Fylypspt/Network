f6665347f0c68608f241c0e5bdea206428503da6
Fylypspt <fylypspt@gmail.com>
2026-09-01 23:13:04 +0200

    Implemented 3-state receiver with preamble and frame size

--- Main.py
+++ Main.py
@@ -5,75 +5,146 @@ import time
 testString = "Hey"
 recData = []
 
+startSeq = [1,1,1,1,1,1,1,1]
+
 cable = 0
+
+def binToDec(bin):
+    #split into 8-bit blocks
+    finalbin = []
+    for i in range(0, len(bin), 8):
+        bloco = bin[i:i+8]
+        finalbin.append(bloco)
+
+    storedMultiple = []
+
+    #convert each block to decimal
+    for letter in finalbin:
+        firstMulti = []
+        for i, val in enumerate(letter):
+            if val == 1:                    
+                firstMulti.append(multiples[i])
+        storedMultiple.append(sum(firstMulti))
+
+    return storedMultiple
+
+def decToBin(dec): #!!!
+    binary = []
+    
+    #build binary big to small
+    for mult in multiples:
+        if dec >= mult:
+            binary.append(1)
+            dec -= mult
+        else:
+            binary.append(0)
+            
+    return binary
 
 def ASCIIConv(info, work):
     if work == True:
+        #text -> binary
         letters = list(info)
         finalBinary = []
         cleanBinary = []
+
         for letter in letters:
             decimal_num = char_map[letter]
             binaryResult = []
             
+            #convert char to 8-bit binary
             for v in multiples:
                 if decimal_num >= v:
                     binaryResult.append(1)
                     decimal_num -= v
                 else:
                     binaryResult.append(0)
+
             finalBinary.append(binaryResult)
     
+        #flatten binary lists
         for letter in finalBinary:
             for bit in letter:
                 cleanBinary.append(bit)
                     
         return cleanBinary
+
     else:
+        #binary -> text
         dataL = []
-        finalbin = []
         data = ""
-        for i in range(0, len(info), 8):
-            bloco = info[i:i+8]
-            finalbin.append(bloco)
+        storedMultiple = binToDec(info)
 
-        storedMultiple = []
-
-        for letter in finalbin:
-            firstMulti = []
-            for i, val in enumerate(letter):
-                if int(val) == 1:                    
-                    firstMulti.append(multiples[i])
-            storedMultiple.append(sum(firstMulti))
-    
+        #convert decimals to chars
         for index, mul in enumerate(storedMultiple):
             for l, v in char_map.items():
                 if v == storedMultiple[index]:
                     dataL.append(l)
+
+        #join chars
         for letter in dataL:
             data += str(letter)
 
         return data
 
 def startClock():
-    global cable, recData
-    data = []
-    iteration = 0
+    global cable
+    lastSeq = []
     time.sleep(tick / 2)
+    data = []
 
-    while iteration < maxBits:
-        data.append(cable)
-        iteration += 1
+    #state 0
+    while not (lastSeq == startSeq):
         time.sleep(tick)
-    recData = data
-    return recData
+        lastSeq.append(cable)
+
+        #keep only the last 8
+        if len(lastSeq) > 8:
+            lastSeq.pop(0)
+
+    #state 1
+    SizeBinary = []
+
+    #read data size
+    for i in range(8):
+        time.sleep(tick)
+        SizeBinary.append(cable)
+
+    size = binToDec(SizeBinary)[0]
+
+    #state 2
+    #read actual data
+    for i in range(size*8):
+        time.sleep(tick)
+        data.append(cable)
+
+    return data
+    
 
 def sendData(data):
-    global cable, recData
-    
+    global cable
+
+    time.sleep(tick * 10) #testing silence
+
+    #start
+    for bit in startSeq:
+        cable = bit
+        time.sleep(tick)
+
+    #size
+    binarySize = decToBin(int(len(data)/8))
+
+    for bit in binarySize:
+        cable = bit
+        time.sleep(tick)
+
+    #data
     for bit in data:
         cable = bit
         time.sleep(tick)
+
+    #reset cable
+    cable = 0
 
 def transmit(data):
     with ThreadPoolExecutor() as executor:
@@ -82,7 +153,6 @@ def transmit(data):
 
     binary = res.result()
 
-    cable = 0
     return ASCIIConv(binary, False)
 
 def test():
